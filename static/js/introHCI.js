e82f1380439bc9bebd61bd5232fc88ad1d12624c
Yujie Wang <Yuw349@ucsd.edu>
2018-01-18 09:52:15 -0800

    updated

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -27,7 +27,7 @@ function projectClick(e) { 
 
     $(this).css("background-color", "#7fff00");
 
-    var projectTitle = $(this).find("p").text();
-    var jumbotronHeader = $("#jumbotron h1");
-    jumbotronHeader.text(projectTitle);
+    //var projectTitle = $(this).find("p").text();
+    //var jumbotronHeader = $("#jumbotron h1");
+    //jumbotronHeader.text(projectTitle);
 }
